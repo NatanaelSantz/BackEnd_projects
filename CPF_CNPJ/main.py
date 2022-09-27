20a4373fb3ee801622ff6d732d2452239a4fcb11
NatanaelSantz <santosnatanael553@gmail.com>
2022-09-27 09:54:40 -0300

    feat: identifying type of doc

--- CPF_CNPJ/main.py
+++ CPF_CNPJ/main.py
@@ -1,13 +1,21 @@
 from cpf_cnpj import Document
-cpf= input("digite seu CPF:")
-cpf = str(cpf)
-cnpj_t = input("digite seu CNPJ:")
-cnpj = str(cnpj_t)
+type_doc = input("Digite o tipo de documento: ").upper()
+if type_doc == "CPF":
+  cpf= input("digite seu CPF:")
+  cpf = str(cpf)
+  document1 = Document.create_document(cpf)
+  print(f"CPF: %s" % document1)
+elif type_doc == "CNPJ":
+  cnpj_t = input("digite seu CNPJ:")
+  cnpj = str(cnpj_t)
+  document2 = Document.create_document(cnpj)
+  print(f"CNPJ: %s" % document2)
+else:
+  print("Unknown type doc: %s" % type_doc)
 
-document1 = Document.create_document(cpf)
-document2 = Document.create_document(cnpj)
-print(f"""CPF:{document1} 
-CNPJ:{document2}""")
+
+
+
 
 # 10630743000107
 # 50440192838
