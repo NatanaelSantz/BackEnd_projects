be43ed7e8f206ac8f3bdda808c51d30459950b22
NatanaelSantz <santosnatanael553@gmail.com>
2022-09-26 23:40:46 -0300

    refactor code

--- CPF_CNPJ/main.py
+++ CPF_CNPJ/main.py
@@ -7,7 +7,7 @@ cnpj = str(cnpj_t)
 document1 = Document.create_document(cpf)
 document2 = Document.create_document(cnpj)
 print(f"""CPF:{document1} 
-      CNPJ:{document2}""")
+CNPJ:{document2}""")
 
 # 10630743000107
 # 50440192838
