87c25d5a67464c312155ed7a4e670b3ea1cdeaa8
NatanaelSantz <santosnatanael553@gmail.com>
2022-09-26 23:32:07 -0300

    get the user's cpf and cnpj

--- CPF_CNPJ/main.py
+++ CPF_CNPJ/main.py
@@ -1,7 +1,7 @@
 from cpf_cnpj import Document
-cpf= 50440192838
+cpf= input("digite seu CPF:")
 cpf = str(cpf)
-cnpj_t = 45021766000191
+cnpj_t = input("digite seu CNPJ: ")
 cnpj = str(cnpj_t)
 
 document = Document.create_document(cnpj)
